a9c59c9da3755be765b0deb88ee77d7e6eaa560f
Jakub Niekraszewicz <niekraszewiczjakub@proton.me>
2025-10-23 19:11:52 +0200

    Popraw kamere
    - Kamera oddala sie gdy patrzymy na stol, lerp przy zmianie promienia
    - Kamery nie mozna obnizyc ponizej stolu (MAX_PHI)

--- camera_3d.gd
+++ camera_3d.gd
@@ -1,15 +1,19 @@
 extends Camera3D
 
 @export var target: Node3D
-@export var mouse_sensitivity = 0.005
-@export var radius = 5.0
+@export var mouse_sensitivity = 0.003
+@export var table_camera_radius = 13.0
+@export var ball_camera_radius = 5.0
+@export var camera_speed = 10
 
 const MIN_PHI = 0.25
-const MAX_PHI = PI - 1.0
+const MAX_PHI = 1.45
 var theta = 0.0
 var phi = 1.0
 
-var ball_list;
+var ball_list
+var camera_current_radius
+var camera_target_radius
 var current_target_index: int = -1
 var offset = Vector3(0.0, 0.0, 0.0)
 var pivot = Vector3.ZERO
@@ -19,11 +23,16 @@ func _ready() -> void:
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	self.position = Vector3.ZERO
 	ball_list = get_tree().get_nodes_in_group("balls")
+	
+	camera_current_radius = table_camera_radius
+	camera_target_radius = camera_current_radius
 
-func _process(delta: float) -> void:	
-	var x = radius * sin(phi) * cos(theta)
-	var y = radius * cos(phi)
-	var z = radius * sin(phi) * sin(theta)
+func _process(delta: float) -> void:
+	camera_current_radius = lerp(camera_current_radius, camera_target_radius, camera_speed * delta)
+	
+	var x = camera_current_radius * sin(phi) * cos(theta)
+	var y = camera_current_radius * cos(phi)
+	var z = camera_current_radius * sin(phi) * sin(theta)
 	offset = Vector3(x, y, z)
 	
 	var target_center = Vector3.ZERO
@@ -31,7 +40,7 @@ func _process(delta: float) -> void:
 		target_center = target.global_position
 		
 	if animating:
-		pivot = lerp(pivot, target_center, 0.2)
+		pivot = lerp(pivot, target_center, camera_speed * delta)
 
 		if pivot.distance_to(target_center) < 0.01:
 			animating = false
@@ -60,9 +69,12 @@ func update_camera_target():
 		var total_targets = ball_list.size() + 1
 		current_target_index = wrapi(current_target_index, 0, total_targets)
 		
+		# Scene center
 		if current_target_index == ball_list.size():
+			camera_target_radius = table_camera_radius
 			target = null
 		else:
+			camera_target_radius = ball_camera_radius
 			target = ball_list[current_target_index]
 
 		animating = true
